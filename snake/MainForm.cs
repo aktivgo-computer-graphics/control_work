60f386b81e2d6703e7d331db023d2dc87dd6a08b
aktivgo <aktivgo01062001@gmail.com>
2022-03-25 10:22:51 +0300

    Init project

---
+++ snake/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake
+{
+    public partial class MainForm : Form
+    {
+        private Graphics Graph;
+        private Pen MyPen;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            Graph = CreateGraphics();
+            Graph.SmoothingMode = SmoothingMode.HighQuality;
+            MyPen = new Pen(Color.Black);
+        }
+        
+        
+    }
+}
