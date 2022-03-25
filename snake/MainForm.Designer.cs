392c2195ce0f382c0b702aa69934796ee27c17aa
aktivgo <aktivgo01062001@gmail.com>
2022-03-25 11:35:56 +0300

    Process

--- snake/MainForm.Designer.cs
+++ snake/MainForm.Designer.cs
@@ -31,7 +31,35 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer = new System.Windows.Forms.Timer(this.components);
+            this.startBtn = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // startBtn
+            // 
+            this.startBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.startBtn.Location = new System.Drawing.Point(12, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(100, 33);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.stopBtn.Location = new System.Drawing.Point(118, 12);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(100, 33);
+            this.stopBtn.TabIndex = 0;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.stopBtn_Click);
             // 
             // MainForm
             // 
@@ -39,12 +67,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startBtn);
             this.DoubleBuffered = true;
             this.Name = "MainForm";
             this.Text = "Snake";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Button stopBtn;
+
+        private System.Windows.Forms.Button startBtn;
 
         private System.Windows.Forms.Timer timer;
 
--- snake/MainForm.cs
+++ snake/MainForm.cs
@@ -14,16 +14,142 @@ namespace snake
     public partial class MainForm : Form
     {
         private Graphics Graph;
-        private Pen MyPen;
+        private Pen BlackPen;
+        private Pen WhitePen;
+
+        private Queue<Point> snake;
+        private Point head;
         
         public MainForm()
         {
             InitializeComponent();
             Graph = CreateGraphics();
             Graph.SmoothingMode = SmoothingMode.HighQuality;
-            MyPen = new Pen(Color.Black);
+            BlackPen = new Pen(Color.Black, 2);
+            WhitePen = new Pen(Color.White, 2);
+
+            snake = new Queue<Point>();
         }
         
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            StartSnake();
+        }
         
+        private void stopBtn_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+        }
+
+        private void StartSnake()
+        {
+            var barriers = CreateBarriers();
+            PaintBarriers(barriers);
+            
+            snake = CreateSnake(5);
+            PaintSnake(snake);
+            
+            timer.Start();
+        }
+        
+        private List<GraphicsPath> CreateBarriers()
+        {
+            var barriers = new List<GraphicsPath>();
+            
+            var barrier = new List<Point>
+            {
+                new(100, 100),
+                new(300, 100),
+                new(300, 300),
+                new(100, 300)
+            };
+            var path = new GraphicsPath();
+            path.AddLines(barrier.ToArray());
+            path.CloseAllFigures();
+            barriers.Add(path);
+            
+            barrier = new List<Point>
+            {
+                new(500, 300),
+                new(700, 300),
+                new(700, 700),
+                new(600, 700),
+                new(600, 500),
+                new(500, 500),
+            };
+            path = new GraphicsPath();
+            path.AddLines(barrier.ToArray());
+            path.CloseAllFigures();
+            barriers.Add(path);
+
+            return barriers;
+        }
+
+        private void PaintBarriers(List<GraphicsPath> barriers)
+        {
+            foreach (var barrier in barriers)
+            {
+                Graph.DrawPath(BlackPen, barrier);
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            Step();
+        }
+
+        private Queue<Point> CreateSnake(int size)
+        {
+            var s = new Queue<Point>();
+
+            for (var i = 0; i < size; i++)
+            {
+                var p = new Point(70 - i * 10, 100);
+                s.Enqueue(p);
+
+                if (i == size - 1)
+                {
+                    head = p;
+                }
+            }
+
+            return s;
+        }
+
+        private void PaintSnake(Queue<Point> s)
+        {
+            var size = s.Count;
+            for (var i = 0; i < size; i++)
+            {
+                var tail = s.Dequeue();
+                Graph.DrawRectangle(BlackPen, tail.X - 2, tail.Y - 2, 4, 4);
+            }
+        }
+
+        private void Step()
+        {
+            var tail = snake.Dequeue();
+            ClearPart(tail);
+            tail.X = head.X + 10;
+            snake.Enqueue(tail);
+            PaintPart(tail);
+        }
+
+        private void PaintPart(Point part)
+        {
+            Graph.DrawRectangle(WhitePen, part.X - 2, part.Y - 2, 4, 4);
+        }
+
+        private void ClearPart(Point part)
+        {
+            Graph.DrawRectangle(WhitePen, part.X - 2, part.Y - 2, 4, 4);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Graph.Dispose();
+            BlackPen.Dispose();
+            timer.Dispose();
+        }
     }
 }
